c90cc6f86c2605be874cf3dd051a167725cb416f
lcc1016 <76151102+lcc1016@users.noreply.github.com>
2020-12-17 16:03:46 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://lcc1016.github.io/testfrog/model.json';
 
 
 function preload() {
